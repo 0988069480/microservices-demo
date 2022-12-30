59c84ba0070ca297bbf2f28acfb30b61b5301ab8
Nguyễn Việt Anh <nguyenvietanh24101999@gmail.com>
2022-12-30 22:32:49 +0700

    spring cloud microservices commit#1

---
+++ microservices/src/main/java/com/example/springmicroservicescommon/SpringmicroservicesCommonApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmicroservicescommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmicroservicesCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmicroservicesCommonApplication.class, args);
+    }
+
+}
